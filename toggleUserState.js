f26c550b731a4de398742f0ad76f66036dd50ac5
Amphy-nick <D_burkovskui@ukr.net>
2021-08-25 22:28:44 +0300

    update

--- toggleUserState.js
+++ toggleUserState.js
@@ -8,14 +8,13 @@
  
  const toggleUserState = (allUsers, userName) => {
     
-    const promise = new Promise(resolve => {
+    return new Promise(resolve => {
     const updatedUsers = allUsers.map(user =>
      user.name === userName ? { ...user, active: !user.active } : user,
     );
    resolve(updatedUsers)
     });
  
-   return promise
  };
  
  const logger = updatedUsers => console.table(updatedUsers);
